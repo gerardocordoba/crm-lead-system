7174ffafd9113cb738b72744a003542c64b45cb5
Gerardo Cordoba <gerardo.cordoba26@gmail.com>
2026-06-29 11:19:57 +0200

    week 1 - variables practice with CRM lead data

---
+++ practice/variables.py
@@ -0,0 +1,16 @@
+# CRM Lead Variables
+
+cif = 123
+name = "Carlos Nunez"
+product = "POS"
+segment = "Micro"
+municipio = "San Pedro Sula"
+departamento = "Cortés"
+commercial_name = "Patitos Store"
+informal_business = True
+monthly_sales = 10000.00
+
+print(f"{name} company {commercial_name} is located in {municipio}, {departamento} and has monthly sales of ${monthly_sales}")
+print(f"it is a {segment} business that wants a {product}")
+print(f"Informal business: {informal_business}")
+
