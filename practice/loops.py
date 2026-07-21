d4be6bc8e7d14e1f110158798fe4e37892f10b84
Gerardo Cordoba <gerardo.cordoba26@gmail.com>
2026-07-21 11:57:25 +0200

    loops practice - pin extractor

---
+++ practice/loops.py
@@ -0,0 +1,13 @@
+
+
+def pin_extractor(poems):
+    secret_code = ''
+    for poem in poems:
+        lines = poem.split('\n')
+        for line_index, line in enumerate(lines):
+            words = line.split()
+            if len(words) > line_index:
+                secret_code += str(len(words[line_index]))
+            else:
+                secret_code += '0'
+        return secret_code
